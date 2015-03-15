8015ebc71aeb0e559c18e4ba8594950f9e519e0d
Dave Kliczbor <maligree@gmx.de>
2015-03-15 23:24:36 +0100

    More comments in filters.py!

--- filters.py
+++ filters.py
@@ -4,7 +4,7 @@ Deny = False
 
 # Do not speak any tweets that contain any of the following keys:
 FILTER_BY_KEY_PRESENT = [
-  'retweeted_status',
+  'retweeted_status',  			# Do not speak RTs
 ]
 
 # Each rule has the format ('key', 'pattern', Action)
@@ -13,23 +13,23 @@ FILTER_BY_KEY_PRESENT = [
 # If a 'default' key is encountered, the Action there is used immediately.
 # The rules are evaluated in the order written here, starting with the first.
 FILTER_BY_KEY_VALUE = [
-  ('lang', 'en', Allow),
-  ('lang', 'de', Allow),
-  ('lang', 'jp', Deny),
+  ('lang', 'en', Allow),		# English is ok
+  ('lang', 'de', Allow),		# Yes, I know german
+  ('lang', 'jp', Deny),			# I cannot speak japanese.
   ('default', Allow)
 ]
 
 # Same as above, but here the key values are in a dict. And we have regex patterns.
 FILTER_BY_REGEX_MATCH = {
   'text': [
-    ('^http[^ ]+$', Deny),
+    ('^http[^ ]+$', Deny),		# Don't speak link-only tweets
   ],
   'default': Allow
 }
 
 # Simple replacements
 REPLACE_TEXT_SIMPLE = {
-  u'<3': u'KleinerDrei',
+  u'<3': u'KleinerDrei',		# <3
 }
 
 REPLACE_TEXT_REGEX = {
